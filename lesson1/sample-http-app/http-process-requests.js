d8cc18601babadf8f8d800b5bdcd3bd7dd27b97c
Valentin Bojinov <v.bojinov@seeburger.com>
2016-10-03 14:36:22 +0300

    Sample http application handling all known http methods

---
+++ lesson1/sample-http-app/http-process-requests.js
@@ -0,0 +1,101 @@
+var http = require('http');
+var url = require('url');
+var port = 8180;
+
+function processGetRequest(request, response) {
+  response.writeHead(200, {
+    'Content-Type' : 'application/json'
+  });
+  var query = url.parse(request.url, true).query;
+  var object = {};
+  for (var arg in query)
+  {	  
+	  object[arg] = query[arg];
+	  console.log(arg + ':' + query[arg]);	  
+  }
+  
+  response.end(JSON.stringify(object));
+}
+
+function processPostRequest(request, response) {
+  response.writeHead(200, {
+    'Content-Type' : 'application/json'    	
+  });
+  
+  var data = '';
+  request.on('data', function(chunk) {
+	  data += chunk;
+	    	  
+  });
+  request.on('end', function(){
+	  
+		  console.log(data);	  
+		  
+		  var object = JSON.parse(data);
+		  object.processed = true;		  
+		  response.end(JSON.stringify(object));
+	  
+  });
+  request.on('error', function(err){
+	  console.log('error');
+  })
+}
+
+function processPutRequest(request, response) {
+  response.writeHead(200, {
+    'Content-Type' : 'text/plain'
+  });
+  response.end('Put action was requested');
+}
+
+function processHeadRequest(request, response) {
+  response.writeHead(200, {
+    'Content-Type' : 'text/plain'
+  });
+  response.end('Head action was requested');
+}
+
+function processDeleteRequest(request, response) {
+  response.writeHead(200, {
+    'Content-Type' : 'text/plain'
+  });
+  response.end('Delete action was requested');
+}
+
+function processBadRequest(response) {
+  response.writeHead(400, {
+    'Content-Type' : 'text/plain'
+  });
+  response.end('Bad request');
+}
+
+function processRequest(request, response) {
+	
+  console.log(request.method + ' ' + request.url);
+  
+  switch (request.method) {
+    case 'GET':
+      processGetRequest(request, response);
+      break;
+    case 'POST':
+      processPostRequest(request, response);
+      break;
+    case 'PUT':
+      processPutRequest(request, response);
+      break;
+    case 'DELETE':
+      processDeleteRequest(request, response); 
+      break;
+    case 'HEAD':
+      processHeadRequest(request, response);
+      break;
+    default:
+      processBadRequest(response);
+      break;
+  }
+  console.log('Request processing ended');
+}
+
+http.createServer(processRequest).listen(port, '127.0.0.1');
+
+console.log('Started Node.js http server at http://127.0.0.1:' + port);
