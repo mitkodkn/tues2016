e908923ff6f50c935669e9ba57844cdb419c2155
valio <valio@G771JW>
2016-11-08 23:27:02 +0200

    fix

--- app/modules/image-provider.js
+++ app/modules/image-provider.js
@@ -19,12 +19,8 @@ exports.provideImage = function(response)
 {
 	response.writeHead(200, {
 		'Content-Type' : 'image/jpeg'
-	});
-	
-<<<<<<< HEAD
-	readImage('./images/image	.jpg', response);	
-=======
+	});	
+
 	readImage('./images/image.jpg', response);	
->>>>>>> branch 'master' of https://github.com/valiobojinov/tues2016
 };
 
